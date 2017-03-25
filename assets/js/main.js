cbf871b2fe3f092f4f22f780b67d164c9254f408
katiechin <kchin@helloelephant.com>
2017-03-25 19:07:26 -0400

    sticky filter bar in gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,24 @@
 
 console.log('we got us some javascript');
 
+var h = document.getElementById("filter");
+var stuck = false;
+var stickPoint = getDistance();
+
+function getDistance() {
+  var topDist = h.parentNode.parentNode.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageYOffset;
+  if ( (distance <= 0) && !stuck) {
+    h.classList.add("sticky");
+    stuck = true;
+  } else if (stuck && (offset <= stickPoint)){
+    h.classList.remove("sticky");
+    stuck = false;
+  }
+}
 }());
