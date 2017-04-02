fabba218ffd8b636ebd07f3d9432cea30f7cddf9
katiechin <kchin@helloelephant.com>
2017-04-02 01:05:08 -0400

    made about page responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,38 @@
-import imageUpload from './imageUpload';
-import getSigns from './getSigns';
+// import imageUpload from './imageUpload';
+// import getSigns from './getSigns';
 
-imageUpload();
-getSigns();
+// imageUpload();
+// getSigns();
 
+// Gallery URL Button
 
-console.log('we got us some javascript');
+if(document.getElementById("gallery")) {
+  var urlBtn = document.getElementsByClassName("get-url");
 
-window.addEventListener("DOMContentLoaded", function() {
+  for (var i = 0; i < urlBtn.length; i++) {
+      urlBtn[i].addEventListener('click', function() {
+          var after = this.nextSibling;
+          var urlField = after.nextSibling;
+          urlField.classList.add("visible");
+      }, false);
+  };
+}
 
+// Mobile Nav
 
-var urlBtn = document.getElementsByClassName("get-url");
+var menu = document.getElementById("mobile-menu");
+var closeIcon = document.getElementById("nav-close");
+var hamburgerIcon = document.getElementById("mobile-hamburger");
 
-for (var i = 0; i < urlBtn.length; i++) {
-    urlBtn[i].addEventListener('click', function() {
-        var after = this.nextSibling;
-        var urlField = after.nextSibling;
-        urlField.classList.add("visible");
-    }, false);
-};
+closeIcon.addEventListener('click', function() {
+  menu.classList.add("_hide");
+}, false);
 
-document.getElementById("nav-close").addEventListener('click', function() {
-      document.getElementById("mobile-menu").classList.add("_hide");
-  }, false);
+hamburgerIcon.addEventListener('click', function() {
+  menu.classList.remove("_hide");
+}, false);
 
-document.getElementById("mobile-hamburger").addEventListener('click', function() {
-      document.getElementById("mobile-menu").classList.remove("_hide");
-  }, false);
+// Sticky gallery filter
 
 if (document.getElementById("filter")) {
   var h = document.getElementById("filter");
@@ -50,6 +56,9 @@ if (document.getElementById("filter")) {
     }
   }
 }
+
+// Form file uploader
+
 if(document.getElementById("file")) {
   document.getElementById("file").onchange = function () {
     var input = document.getElementById("file");
@@ -69,4 +78,3 @@ if(document.getElementById("file")) {
     }
   }
 }
-}, false);
